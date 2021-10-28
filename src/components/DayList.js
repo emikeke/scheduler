fe736f2542f82e580a1cc3f10c4bdb4d4ec0ceb1
Emily K <emily.ke@hotmail.com>
2021-10-28 03:23:40 -0400

    controlled list re-edit daylistitem + interviewerlist

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,11 +5,11 @@ import "components/DayListItem.scss";
 export default function DayList(props){
   const daylist = props.days.map(eachDay => (
     <DayListItem
-      key={eachDay.id}
-      name={eachDay.name}
-      spots={eachDay.spots}
-      selected={eachDay.name === props.day}
-      setDay={props.setDay}
+      key={props.id}
+      name={props.name}
+      spots={props.spots}
+      selected={props.name === props.value}
+      setDay={props.onChange}
     />
   ));
   return(
