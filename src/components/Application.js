c9242f71a5ca1cc58291f251fc23c7f2090d99db
Emily K <emily.ke@hotmail.com>
2021-11-02 12:58:24 -0400

    eslint

--- src/components/Application.js
+++ src/components/Application.js
@@ -33,33 +33,35 @@ export default function Application(props) {
 	);
   return (
     <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
+    <section className="sidebar">
+      <img
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu">
+      <DayList
+        days={state.days}
+        day={state.day}
+        setDay={setDay}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+      />
+      </nav>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
+    </section>
+    <section className="schedule">
+      {appointments}
+        <Appointment key="last" time="5pm" 
+          bookInterview={bookInterview} 
+          cancelInterview={cancelInterview} 
         />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-        <DayList
-          days={state.days}
-          day={state.day}
-          setDay={setDay}
-          bookInterview={bookInterview}
-          cancelInterview={cancelInterview}
-        />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>
-      <section className="schedule">
-        {appointments}
-        <Appointment key="last" time="5pm" bookInterview={bookInterview} cancelInterview={cancelInterview} 
-        />
-      </section>
+    </section>
     </main>
   );
 }
