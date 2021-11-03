d8f9539652c1e333b1e937c9a9f2b332ebe650a5
Emily K <emily.ke@hotmail.com>
2021-11-02 21:39:45 -0400

    test 1

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -23,5 +23,6 @@ export default function InterviewerList(props) {
 }
 
 InterviewerList.propTypes = {
-	interviewers: PropTypes.array.isRequired
+	value: PropTypes.number,
+	onChange: PropTypes.func.isRequired
 };
